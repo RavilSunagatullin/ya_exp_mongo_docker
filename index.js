7c04844ad069e244e970130239f5ca7057b5b3df
RSA <ravils465@gmail.com>
2024-04-28 20:45:15 +0300

    commit

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const express = require('express');
-const config = require('./src/config/index');
-const mongoose = require('./src/database/index');
-
-const app = express();
+const express = require('express')
+const config = require('./src/config/index')
+const mongoose = require('./src/database/index')
+const bodyParser = require('body-parser')
+const path = require('path')
+const mainRouter = require('./src/routes/main')
+const app = express()
 
 app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).send('Something broke!');
-});
+	console.error(err.stack)
+	res.status(500).send('Something broke!')
+})
 
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'src', 'public')))
+
+app.use(mainRouter)
 app.listen(config.app.port, () => {
-	console.log(`Server is running on port ${config.app.port}`);
-});
+	console.log(`Server is running on port ${config.app.port}`)
+})
+module.exports = app
---
+++ src/__tests__/server.test.js
@@ -0,0 +1,6 @@
+const sum = require('../utils/test')
+
+test("Test util sum", async () => {
+	expect(sum(5, 5)).toBe(10);
+	expect(sum(3, 5)).toBe(8);
+});
